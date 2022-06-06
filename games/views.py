227c41f4aa99b576a263ae7a6498e3d151a41eab
osboxes.org <cloudygame2022@gmail.com>
2022-06-06 08:29:11 -0400

    Messing with GameHTML 7

--- games/views.py
+++ games/views.py
@@ -6,11 +6,11 @@ def AllGames(request):
     return render(request, 'index.html', { 'games' : Game.objects.all() })
 
 def GameHTML(request, game_name):
+    if (not Game.objects.exists(name=game_name)):
+        return render(request, 'index.html', { 'games' : Game.objects.all() })
+
     game_ = Game.objects.get(name=game_name)
 
     game_songs = Song.objects.filter(game=game_).order_by('ost_index')
-
-    if (game_songs.count == 0):
-        return render(request, 'index.html', { 'games' : Game.objects.all() })
 
     return render(request, 'songs.html', { 'game_name' : game_name, 'all_songs' : game_songs })
