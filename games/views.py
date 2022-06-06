a932036b2fa1307fc45308b41c31f770b78ef776
osboxes.org <cloudygame2022@gmail.com>
2022-06-06 08:36:56 -0400

    Fixed Search Bar

--- games/views.py
+++ games/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Game, Song
 
@@ -9,8 +9,8 @@ def GameHTML(request, game_name):
     if (not Game.objects.filter(name=game_name).exists()):
         return render(request, 'index.html', { 'games' : Game.objects.all() })
 
-    game_ = Game.objects.get(name=game_name)
+    searched_game = Game.objects.get(name=game_name)
 
-    game_songs = Song.objects.filter(game=game_).order_by('ost_index')
+    game_songs = Song.objects.filter(game=searched_game).order_by('ost_index')
 
     return render(request, 'songs.html', { 'game_name' : game_name, 'all_songs' : game_songs })
