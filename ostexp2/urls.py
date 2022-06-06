8066595ce35f74b912c104c2911423cb0beafa5b
osboxes.org <cloudygame2022@gmail.com>
2022-06-06 07:03:47 -0400

    Added redirect when url empty

--- ostexp2/urls.py
+++ ostexp2/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('games/', include('games.urls'))
+    path('games/', include('games.urls')),
+    path('', include('games/games.urls'))
 ]
 
 #TEST CHANGE
