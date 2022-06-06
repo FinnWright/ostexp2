156c206530ee6018a91a848b0deb7335abae3e8c
osboxes.org <cloudygame2022@gmail.com>
2022-06-06 09:42:25 -0400

    Fixed Search Bar 21

--- games/urls.py
+++ games/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.AllGames),
-    path(r'?q=<str:game_name>/', views.GameHTML),
+    re_path(r'?q<str:game_name>/', views.GameHTML),
     path('<str:game_name>/', views.GameHTML)
 ]
