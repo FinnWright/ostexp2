173bed254f10c93e837ac4240c3dadfb23c3cc4c
osboxes.org <cloudygame2022@gmail.com>
2022-06-06 07:27:56 -0400

    Added logging

--- games/views.py
+++ games/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Game, Song
+import logging
 
 def AllGames(request):
-    games = Game.objects.all()
-    return render(request, 'index.html', { 'games' : games})
+    return render(request, 'index.html', { 'games' : Game.objects.all() })
 
 def GameHTML(request, game_name):
     allsongs = Song.objects.all()
@@ -16,7 +16,11 @@ def GameHTML(request, game_name):
 
     game_songs.sort()
 
+    logging.basicConfig(level=logging.INFO)
+    logging.debug(game_name + game_songs.count)
+
+    #Problem Area:
     if (game_songs.count == 0):
         return render(request, 'index.html', { 'games' : Game.objects.all() })
-        
+
     return render(request, 'songs.html', { 'game_name' : game_name, 'all_songs' : game_songs})
