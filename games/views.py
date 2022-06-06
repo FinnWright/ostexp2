3f3792fe987d73d99d79dd49e8b5f44be1926f64
osboxes.org <cloudygame2022@gmail.com>
2022-06-06 08:06:08 -0400

    Messing with GameHTML

--- games/views.py
+++ games/views.py
@@ -1,26 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Game, Song
-import logging
 
 def AllGames(request):
     return render(request, 'index.html', { 'games' : Game.objects.all() })
 
 def GameHTML(request, game_name):
-    allsongs = Song.objects.all()
-    game_songs = []
+    game_ = Game.objects.get(name=game_name)
 
-    for song in allsongs:
-        if song.game.name == game_name:
-            game_songs.append(song)
+    game_songs = get_object_or_404(Song, game=game_)
 
     game_songs.sort()
 
-    logging.basicConfig(level=logging.INFO)
-    logging.debug(game_songs.count)
-
-    #Problem Area:
     if (game_songs.count == 0):
         return render(request, 'index.html', { 'games' : Game.objects.all() })
 
-    return render(request, 'songs.html', { 'game_name' : game_name, 'all_songs' : game_songs})
+    return render(request, 'songs.html', { 'game_name' : game_name, 'all_songs' : game_songs })
