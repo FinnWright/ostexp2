34051688020f916796c82b0f4d6c6a2622ee961b
osboxes.org <cloudygame2022@gmail.com>
2022-06-07 08:52:23 -0400

    Removed Favicon Traces

--- ostexp2/urls.py
+++ ostexp2/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('games.urls')),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")))
+    path('', include('games.urls'))
 ]
 
 #TEST CHANGE
