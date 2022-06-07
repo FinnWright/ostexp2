dc8e8d4d1491f750b8764c4e9ad4cadf4bb3fa0f
osboxes.org <cloudygame2022@gmail.com>
2022-06-07 08:39:51 -0400

    Added Favicon 2

--- games/urls.py
+++ games/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
     path('', views.AllGames),
-    path('<str:game_name>/', views.GameHTML),
-    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
-    )
+    path('<str:game_name>/', views.GameHTML)
 ]
